85a92cdae7e48a004eea6a70aa1005b022ec2993
Richard Soderberg <rsoderberg@gmail.com>
2017-02-07 21:44:49 -0800

    fix bnetsh_port handling

--- config.sh
+++ config.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 . functions.sh
+. local.sh
 
 [[ -n $BNETSH_CONFIG ]] || {
     BNETSH_CONFIG=1
 
-    defined "$BNETSH_PORT" || BNETSH_PORT=8080
-
+    defined "$BNETSH_PORT" || error "BNETSH_PORT not exported"
     defined "$APIAUTHURL" || error "APIAUTHURL not exported"
     defined "$APIURL" || error "APIURL not exported"
     defined "$APIKEY" || error "APIKEY not exported"
-    defined "$BNETUSER" || error "BNETUSER not exported"
-    defined "$BNETPASS" || error "BNETPASS not exported"
 }
